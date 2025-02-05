896c3dc246a0b32a2087fae9a58c3a817987684b
Mariana Valderrama Castañeda <mvaderrc1@eafit.edu.co>
2025-02-05 12:28:34 -0500

    busqueda de peliculas

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -7,10 +7,13 @@ from .models import Movie
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request,'home.html')
-    return render(request, 'home.html',{'name':'mariana valderrama'})
-    #searchTerm= request.GET.get('searchMovie')
-    #movies = Movie.objects.all()
-    #return render(request,'home.html',{'searchTerm':searchTerm,'movies':movies})
+    #return render(request, 'home.html',{'name':'mariana valderrama'})
+    searchTerm= request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title_icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request,'home.html',{'searchTerm':searchTerm,'movies':movies})
 
 def about(request):
     #return HttpResponse('<h1>Welcome to About Page</h1>')
