9b2bdf467e5391be4446fbd8657277d56e4914d3
Mariana Valderrama Castañeda <mvaderrc1@eafit.edu.co>
2025-02-05 12:46:09 -0500

    visualizacion de peliculas con estilo

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -10,7 +10,7 @@ def home(request):
     #return render(request, 'home.html',{'name':'mariana valderrama'})
     searchTerm= request.GET.get('searchMovie')
     if searchTerm:
-        movies = Movie.objects.filter(title_icontains=searchTerm)
+        movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
     return render(request,'home.html',{'searchTerm':searchTerm,'movies':movies})
